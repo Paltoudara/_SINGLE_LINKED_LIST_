20831fe59419fc2f4757be7cb0ed965a410de165
panagiotis dimitropoulos <162046043+Paltoudara@users.noreply.github.com>
2025-09-08 16:41:20 +0300

    Update List.h

--- List.h
+++ List.h
@@ -585,7 +585,7 @@ private:
 		}
 		catch (...) {
 			delete ptr;//no memory leak
-			throw 1;
+			std::abort();
 		}
 		//whatever left we just give it because we now have only onel list
 		if (curr1 == nullptr && curr2 != nullptr) {
